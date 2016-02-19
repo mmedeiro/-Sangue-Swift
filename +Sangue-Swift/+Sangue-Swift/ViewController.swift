71919254731146fc45906a513603a59751b1004c
mmedeiro <31313132@mackezista.com.br>
2016-02-19 11:06:27 -0200

    Projeto do Xcode

---
+++ +Sangue-Swift/+Sangue-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  +Sangue-Swift
+//
+//  Created by Mariana Medeiro on 19/02/16.
+//  Copyright © 2016 Mariana Medeiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
